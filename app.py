b908626899f35253cc88c52e9da7d5a97be92484
Riosky <duodecimn@qq.com>
2022-12-31 01:37:19 +0800

    Build other primary templates

--- app.py
+++ app.py
@@ -1,7 +1,66 @@
-from flask import Flask, render_template
+import os
 
+from flask import Flask, flash, redirect, render_template, request, session
+from tempfile import mkdtemp
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import login_required
+
+# Configure application
 app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+app.secret_key = '\xca\x0c\x86\x04\x98@\x02b\x1b7\x8c\x88]\x1b\xd7"+\xe6px@\xc3#\\'
+
+
+@app.route('/home')
+def home():
+    session.clear()
+    session["user_id"] = "0"
+    return render_template("home.html")
 
 @app.route('/')
 def index():
+    session.clear()
     return render_template("layout.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+    session.clear()
+    if request.method == "GET":
+        return render_template("login.html")
+    else:
+        # TODO
+        return redirect("/home")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    session.clear()
+    if request.method == "GET":
+        return render_template("register.html")
+    else:
+        # TODO
+        return redirect("/home")
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+
+# Fundtions
+@app.route('/add')
+def add():
+    return render_template("add.html")
